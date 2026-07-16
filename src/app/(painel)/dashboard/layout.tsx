8d76aefe177ac577ed2df20ab7a5c74aa4d65a66
Valdeilson-lima <valdeilsonbdl56@gmail.com>
2026-07-15 22:49:11 -0300

    feat: protect all dashboard routes with session check in layout

--- src/app/(painel)/dashboard/layout.tsx
+++ src/app/(painel)/dashboard/layout.tsx
@@ -1,10 +1,18 @@
+import GetSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
 import { SidebarDashboard } from "./_componentes/sidebar";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await GetSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <>
       <SidebarDashboard>{children}</SidebarDashboard>
